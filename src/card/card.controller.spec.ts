592395a3e06e963e405eb2d5710c5ad241bd1653
lebedev-023046 <lebedev.023046@gmail.com>
2025-07-10 15:20:10 +0300

    feat: add basic testing config

--- src/card/card.controller.spec.ts
+++ src/card/card.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CardController } from './card.controller';
+import { CardService } from './card.service';
 
 describe('CardController', () => {
   let controller: CardController;
@@ -7,6 +8,14 @@ describe('CardController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CardController],
+      providers: [
+        {
+          provide: CardService,
+          useValue: {
+            getById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<CardController>(CardController);
