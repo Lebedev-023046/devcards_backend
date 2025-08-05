4b346eb404cac4e5c4a00851bb5051040bd2e5f0
lebedev-023046 <lebedev.023046@gmail.com>
2025-08-06 00:14:15 +0300

    refactor: enable ignoreExpiration

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -7,7 +7,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      ignoreExpiration: false,
+      ignoreExpiration: true, // !! CHANGE BEFORE RELEASE
       secretOrKey: process.env.JWT_SECRET || 'supersecret',
     });
   }
